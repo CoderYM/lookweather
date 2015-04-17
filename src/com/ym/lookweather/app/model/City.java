071a517d89c4887ad745028395710b398901441f
yangmin <yang.min@jucaifu.com>
2015-04-17 17:39:04 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ src/com/ym/lookweather/app/model/City.java
@@ -0,0 +1,43 @@
+package com.ym.lookweather.app.model;
+
+/**
+ * Created by yangmin on 2015/4/17.
+ */
+public class City {
+    private int id;
+    private String cityName;
+    private String cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
