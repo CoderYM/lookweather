9df710a838d30d6c92aeb1db88c0f8aed79ad7b6
yangmin <yang.min@jucaifu.com>
2015-04-17 17:02:14 +0800

    Second commit

---
+++ src/com/ym/lookweather/app/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.ym.lookweather.app.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.ym.lookweather.app.R;
+
+/**
+ * Created by yangmin on 2015/4/17.
+ */
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
